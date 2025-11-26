8e82bd0868e8d23b526d42ff9acf835aaeb46a83
changin <dain0126@naver.com>
2025-11-26 09:53:13 +0900

    thymeleaf test

---
+++ src/main/java/com/changin/shop/controller/ThymeleafController.java
@@ -0,0 +1,4 @@
+package com.changin.shop.controller;
+
+public class ThymeleafController {
+}
--- src/test/java/com/changin/shop/repository/ItemRepositoryTest.java
+++ src/test/java/com/changin/shop/repository/ItemRepositoryTest.java
@@ -2,11 +2,23 @@ package com.changin.shop.repository;
 
 import com.changin.shop.constant.ItemSellStatus;
 import com.changin.shop.entity.Item;
+import com.changin.shop.entity.QItem;
+import com.querydsl.core.BooleanBuilder;
+import com.querydsl.core.Tuple;
+import com.querydsl.jpa.impl.JPAQuery;
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.transaction.annotation.Transactional;
+import org.thymeleaf.util.StringUtils;
 
 import java.time.LocalDateTime;
 import java.util.List;
@@ -17,14 +29,22 @@ public class ItemRepositoryTest {
     @Autowired
     ItemRepository itemRepo;
 
+    @PersistenceContext
+    private EntityManager em;
+
+
     public void createItemList() {
         for(int i=1;i<=10;i++){
+
+            //번갈아가면서 판매 상태 세팅
+            ItemSellStatus st = i<=5 ? ItemSellStatus.SELL : ItemSellStatus.SOLD_OUT;
+
             Item item = Item.builder()
                     .itemNm("0"+i)
                     .price(10000+i)
                     .stockNumber(1)
                     .itemDetail("hello"+i)
-                    .itemSellStatus(ItemSellStatus.SELL)
+                    .itemSellStatus(st)
                     .regTime(LocalDateTime.now())
                     .updateTime(LocalDateTime.now())
                     .build();
@@ -32,6 +52,55 @@ public class ItemRepositoryTest {
             itemRepo.save(item);
         }
     }
+
+    @Test
+    @DisplayName("querydsl predicate executor 테스트")
+    public void querydslTest2(){
+        createItemList();
+        BooleanBuilder booleanBuilder = new BooleanBuilder();
+        QItem item = QItem.item;
+        String itemSellStatus = null;
+
+        String keyName = "hello";
+        int keyPrice = 10000;
+
+        booleanBuilder.and(item.itemDetail.like("%" + keyName + "%"));
+        booleanBuilder.and(item.price.gt(keyPrice));
+
+        //이건 왜 if문을 써야하는가.
+        if(StringUtils.equals(itemSellStatus, ItemSellStatus.SELL)){
+            booleanBuilder.and(item.itemSellStatus.eq(ItemSellStatus.SELL));
+        }
+
+
+        Pageable pageable = PageRequest.of(0,10);
+        Page<Item> page = itemRepo.findAll(booleanBuilder, pageable);
+
+        List<Item> list = page.getContent();
+        list.stream().forEach(System.out::println);
+    }
+
+
+    @Test
+    @DisplayName("querydsl 테스트")
+    public void querydslTest(){
+        createItemList();
+        JPAQueryFactory queryFactory = new JPAQueryFactory(em);
+        QItem qItem = QItem.item;
+
+        JPAQuery<Tuple> result =
+                queryFactory
+                        .select(QItem.item.price, QItem.item.count())
+                        .from(QItem.item)
+                        .where(QItem.item.itemSellStatus.eq(ItemSellStatus.SELL))
+                        .where(QItem.item.itemDetail.like("%1%"))
+                        .groupBy(QItem.item.price);
+
+
+        result.fetch().forEach(System.out::println);
+    }
+
+
 
     @Test
     @DisplayName("JPQL native 테스트")
