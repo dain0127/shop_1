da3f8b0a3cdea060b43ebfaa1f2136678719d3bc
changin <dain0126@naver.com>
2025-11-27 12:29:19 +0900

    pish

--- src/main/java/com/changin/shop/controller/ThymeleafController.java
+++ src/main/java/com/changin/shop/controller/ThymeleafController.java
@@ -1,16 +1,36 @@
 package com.changin.shop.controller;
 
 import com.changin.shop.dto.ItemDto;
+import com.changin.shop.dto.ParamDto;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.geo.Point;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 
 @Controller
+@Slf4j //log찍어주는 라이브러리
 @RequestMapping("/thymeleaf")
 public class ThymeleafController {
+
+    @GetMapping("/ex4")
+    public String ex4(Model model) {
+        return "thymeleaf/ex4";
+    }
+
+    @GetMapping("/ex3")
+    public String ex3(ParamDto paramDto, Model model) {
+
+        log.info("==============="+paramDto);
+        model.addAttribute("dto", paramDto);
+
+        return "thymeleaf/ex3";
+    }
+
 
     @GetMapping("/ex2")
     public String ex2(Model model){
---
+++ src/main/java/com/changin/shop/dto/ParamDto.java
@@ -0,0 +1,14 @@
+package com.changin.shop.dto;
+
+import lombok.*;
+
+@Getter
+@Setter
+@ToString
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class ParamDto {
+    private String name;
+    private int age;
+}
