4d552b84f2cc67c5c35a6cf0c2ac942ed3e55f94
changin <dain0126@naver.com>
2025-12-18 23:44:59 +0900

    add process that transfer env data to property

--- src/main/java/com/changin/shop/ShopApplication.java
+++ src/main/java/com/changin/shop/ShopApplication.java
@@ -1,5 +1,6 @@
 package com.changin.shop;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
@@ -8,7 +9,20 @@ import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 public class ShopApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ShopApplication.class, args);
+        Dotenv dotenv = Dotenv.load();
+        //Property에 읽어온 env 환경 변수 값 등록.
+        dotenv.entries().forEach(e ->
+                System.setProperty(e.getKey(), e.getValue())
+        );
+
+        // 값 읽기
+        String username = dotenv.get("DB_USERNAME");
+        String password = dotenv.get("DB_PASSWORD");
+
+        System.out.println("============> DB_USERNAME: " + username);
+        System.out.println("============> DB_PASSWORD: " + password);
+
+        SpringApplication.run(ShopApplication.class, args);
 	}
 
 }
